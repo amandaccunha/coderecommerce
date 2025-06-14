33c274fd702aa8400d3dd39e787441079aaec221
Amanda Cunha <93945881+amandaccunha@users.noreply.github.com>
2025-06-14 13:41:49 -0300

    subida de arquivo

---
+++ src/pages/CheckoutForm.jsx
@@ -0,0 +1,13 @@
+// src/pages/CheckoutForm.jsx
+import React from 'react';
+
+const CheckoutForm = () => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>Aqui ficará o formulário de finalização da compra.</p>
+    </div>
+  );
+};
+
+export default CheckoutForm;
